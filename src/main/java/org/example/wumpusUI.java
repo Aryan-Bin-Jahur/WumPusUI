44d4fcbdc34031a19382faccdb75281665c92976
aryan <aryan.bin.jahur@gmail.com>
2022-11-07 16:20:19 +0600

    7/11/2022

--- src/main/java/org/example/wumpusUI.java
+++ src/main/java/org/example/wumpusUI.java
@@ -1,41 +1,24 @@
 package org.example;
-
 import java.awt.*;
 
 import javax.swing.JButton;
 
 import javax.swing.JFrame;
 
-
-
 public class wumpusUI{
-
-
-
     public static void main(String[] args) {
-
-
-
-
 
         JFrame frame = new JFrame();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         frame.setSize(500, 500);
-
         frame.setLayout(new GridLayout(10,10,0,0));
-
-
-
-
 
         for(int i=1;i<=100;i++){
             Button button = new Button(Integer.toString(i));
 
             frame.add(button);
         }
-
 
         frame.setVisible(true);
 
@@ -47,20 +30,12 @@ public class wumpusUI{
 
         frame1.setLayout(new GridLayout(10,10,0,0));
 
-
-
-
-
         for(int i=1;i<=100;i++){
             Button button = new Button(Integer.toString(i));
 
             frame1.add(button);
         }
-
-
         frame1.setVisible(true);
-
-
 
     }
 
